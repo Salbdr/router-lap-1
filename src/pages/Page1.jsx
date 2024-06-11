1dcf90c6ac086631b9298eff66b30cd806acffc6
sult <168359467+Salbdr@users.noreply.github.com>
2024-06-11 20:40:37 +0300

    change

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -4,7 +4,7 @@ export default function Page1(){
     return(
         <>
         <Nav/>
-        <div className="hero min-h-screen" style={{backgroundImage: 'url(public/1.png)'}}>
+        <div className="hero min-h-screen" style={{backgroundImage: 'url(./public/1.png)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
   
